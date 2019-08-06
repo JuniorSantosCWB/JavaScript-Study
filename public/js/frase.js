f9b863fecdb06261e77e8b93a06cc8bd1eef9e24
JuniorSantosCWB <jrsantosdev@gmail.com>
2019-08-06 01:02:33 -0300

    implemented button to select specific phrases from the list and button to record the scoreboard in the database.

--- public/js/frase.js
+++ public/js/frase.js
@@ -1,4 +1,5 @@
 $("#botao-frase").click(fraseAleatoria);
+$("#botao-frase-id").click(buscaFrase);
 
 function fraseAleatoria() {
 	$("#spinner").toggle();
@@ -8,7 +9,7 @@ function fraseAleatoria() {
     	$(".erro").show();
     	setTimeout(function(){
     		$(".erro").toggle();
-    	}, 4000);
+    	}, 8000);
 
     })
     .always(function(){
@@ -24,4 +25,30 @@ function trocaFraseAleatoria(data) {
     atualizaTamanhoFrase();
     atualizaTempoInicial(data[numeroAleatorio].tempo);
 
+}
+
+function buscaFrase(){
+	$("#spinner").toggle();
+	var fraseId = $("#frase-id").val();
+	
+	var dados = {id: fraseId};
+	 $.get("http://localhost:3000/frases", dados, trocaFrase)
+    .fail(function(){
+        $("#erro").toggle();
+        setTimeout(function(){
+            $("#erro").toggle();
+        },4000);
+    })
+    .always(function(){
+        $("#spinner").toggle();
+    });
+}
+
+function trocaFrase(data){
+	console.log(data);
+   
+   var frase = $(".frase");
+    frase.text(data.texto);
+    atualizaTamanhoFrase();
+    atualizaTempoInicial(data.tempo);
 }
